e983322cfda5a1b47fcc1d488fae71b7bfdc280e
geetha125 <geethanjali.g@vakilsearch.com>
2023-12-27 11:16:38 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
-  get '/list_users', to:'users#index'  
+  get '/list_users', to:'users#index'   
+
+  root to: 'home#home' 
 
 end
